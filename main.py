765979b7a50b678170274c4ab444eebeb076d334
Liam Gower <lee.gower17@gmail.com>
2023-01-16 21:50:08 +0000

    init

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 
-service = Service("~/PycharmProjects/drivers/chromedriver_mac64/chromedriver")
-op = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=op)
+# Inspired by code from this: https://github.com/caffreit/Park-Run-Scraper
+
+
 
 PARKRUN_ID = 6486341
 
 def get_parkrun_info(parkrunner_id):
 
+    service = Service("~/PycharmProjects/drivers/chromedriver_mac64/chromedriver")
+    op = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=op)
     URL = f"https://www.parkrun.org.uk/parkrunner/{parkrunner_id}/all/"
     driver.get(URL)
-    print(driver.title)
     src = driver.page_source
     soup = bs4.BeautifulSoup(src, "html.parser")
     table = soup.find_all("table")
+    df = pd.read_html(str(table))[2] # There's 3 tables, the 3rd (index=2) is the one with all runs
 
     driver.close()
 
-    return table
+    return df
 
 def get_parkrun_events_list():
     # This website shared a list of parkruns and their lat/longs
@@ -30,13 +33,15 @@ def get_parkrun_events_list():
     return events
 
 
-
+# Get distinct list of parkruns attended
+# Join to parkrun master list to obtain lat and long
+# Plot on Map display :D
 
 if __name__ == '__main__':
 
-    table = get_parkrun_info(PARKRUN_ID)
-    tmp_df = pd.read_html(str(table))[2]
-    print(tmp_df)
+    df = get_parkrun_info(PARKRUN_ID)
+    
+    print(df)
 
     print("=" * 80)
 
